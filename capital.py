74b9d5f6c55bd8c8424876076433b394328e7d58
Ikharona Isaiah <engr.isaiah665@gmail.com>
2022-03-18 13:37:19 +0100

    Written the code

--- capital.py
+++ capital.py
@@ -1,3 +1,11 @@
 def capit_letter(word):
     result = []
-    
+    for item in word:
+        if item.isupper() is True:
+            result.append(item)
+            continue
+
+    return result
+
+
+capit_letter("WiRdEsFC")
